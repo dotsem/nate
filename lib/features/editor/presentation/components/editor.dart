b018445bc686789cca8d491f68bb434db2f3ba96
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-04-16 22:30:54 +0200

    fix double scrollbars & textless side not scrolling

--- lib/features/editor/presentation/components/editor.dart
+++ lib/features/editor/presentation/components/editor.dart
@@ -43,62 +43,70 @@ class _EditorState extends State<Editor> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        LeftSidebar(
-          lineCount: widget.showLineNumbers
-              ? '\n'.allMatches(_controller.text).length + 1
-              : 0, // only calculate when needed
-          showLineNumbers: widget.showLineNumbers,
-          scrollController: _sidebarController,
-        ),
-        Expanded(
-          child: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () => _focusNode.requestFocus(),
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: EditorConfig.topPadding, top: EditorConfig.topPadding),
-              child: Scrollbar(
-                thumbVisibility: true,
-                controller: _horizontalController,
-                notificationPredicate: (n) => n.metrics.axis == Axis.horizontal,
-                child: Scrollbar(
-                  thumbVisibility: true,
-                  controller: _verticalController,
-                  notificationPredicate: (n) => n.metrics.axis == Axis.vertical,
-                  child: SingleChildScrollView(
-                    controller: _horizontalController,
-                    scrollDirection: Axis.horizontal,
-                    child: IntrinsicWidth(
-                      child: TextField(
-                        controller: _controller,
-                        focusNode: _focusNode,
-                        scrollController: _verticalController,
-                        maxLines: null,
-                        minLines: 1,
-                        keyboardType: TextInputType.multiline,
-                        autofocus: true,
-                        style: EditorConfig.textStyle,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          isDense: true,
-                          contentPadding: EdgeInsets.only(
-                            left: 8,
-                            top: EditorConfig.topPadding,
-                            bottom: EditorConfig.bottomPadding,
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          LeftSidebar(
+            lineCount: widget.showLineNumbers ? '\n'.allMatches(_controller.text).length + 1 : 0,
+            showLineNumbers: widget.showLineNumbers,
+            scrollController: _sidebarController,
+          ),
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () => _focusNode.requestFocus(),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: EditorConfig.topPadding, top: EditorConfig.topPadding),
+                    child: Scrollbar(
+                      thumbVisibility: true,
+                      controller: _horizontalController,
+                      notificationPredicate: (n) => n.metrics.axis == Axis.horizontal,
+                      child: Scrollbar(
+                        thumbVisibility: true,
+                        controller: _verticalController,
+                        notificationPredicate: (n) => n.metrics.axis == Axis.vertical,
+                        child: SingleChildScrollView(
+                          controller: _horizontalController,
+                          scrollDirection: Axis.horizontal,
+                          child: IntrinsicWidth(
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(minWidth: constraints.maxWidth),
+                              child: TextField(
+                                controller: _controller,
+                                focusNode: _focusNode,
+                                scrollController: _verticalController,
+                                maxLines: null,
+                                minLines: 1,
+                                keyboardType: TextInputType.multiline,
+                                autofocus: true,
+                                style: EditorConfig.textStyle,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.only(
+                                    left: 8,
+                                    top: EditorConfig.topPadding,
+                                    bottom: EditorConfig.bottomPadding,
+                                  ),
+                                ),
+                                onChanged: widget.onChanged,
+                              ),
+                            ),
                           ),
                         ),
-                        onChanged: widget.onChanged,
                       ),
                     ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
